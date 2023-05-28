13fe0d7d7dfdcd30ae312f6590074ec84bf7c4d3
Luka Lešić <lukalesic20@gmail.com>
2023-05-28 20:33:06 +0200

    refactored linechart

--- linechart/linechart.js
+++ linechart/linechart.js
@@ -1,128 +1,99 @@
- // Set up the dimensions and margins for the chart
- var margin = { top: 20, right: 20, bottom: 30, left: 50 };
- var width = 21000 - margin.left - margin.right;
- var height = 400 - margin.top - margin.bottom;
+ var margin = { top: 20, right: 20, bottom: 0, left: 30 };
+ var width = 20000 - margin.left - margin.right;
+ var height = 500 - margin.top;
 
- // Create the SVG element
  var svg = d3.select("#chart")
-   .attr("width", width + margin.left + margin.right)
-   .attr("height", height + margin.top + margin.bottom)
+ .attr("height", height)
+   .attr("width", width)
    .append("g")
    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
- // Load the data from JSON file
  d3.json("jsondata.json").then(function(data) {
-   // Extract the available years from the data
    var years = Object.keys(data[0]).filter(function(key) {
      return !isNaN(key);
    });
 
-   // Create the year dropdown options
-   var yearSelect = d3.select("#year-select");
-   yearSelect.selectAll("option")
+   var yearsArray = d3.select("#year-select");
+   yearsArray.selectAll("option")
      .data(years)
      .enter()
      .append("option")
      .text(function(d) { return d; });
 
-   // Set up the initial year selection
-   var selectedYear = yearSelect.property("value");
+   var currentActiveYear = yearsArray.property("value");
+   updateLineGraph(currentActiveYear);
 
-
-   // Update the chart based on the selected year
-   updateChart(selectedYear);
-
-   // Handle the year selection change event
-   yearSelect.on("change", function() {
-     selectedYear = yearSelect.property("value");
-     updateChart(selectedYear);
+   yearsArray.on("change", function() {
+     currentActiveYear = yearsArray.property("value");
+     updateLineGraph(currentActiveYear);
    });
 
-   // Function to update the chart based on the selected year
-   function updateChart(year) {
-     // Extract data for the selected year
-     document.getElementById("selected-year").textContent = year;
+   function updateLineGraph(year) {
+     document.getElementById("activeyear").textContent = year;
 
      var yearData = data.map(function(d) {
        return { "Country": d.Country, "Value": d[year], "HDI": d.HDI };
      });
 
-     // Filter out countries with undefined or empty HDI values and incomplete data
      yearData = yearData.filter(function(d) {
-       return d.Value !== undefined && d.Value !== "..";
+       return d.Value !== "??"  && d.Value !== undefined && d.Value !== "..";
      });
 
-     // Sort the data based on HDI value
-     yearData.sort(function(a, b) {
-       return a.HDI - b.HDI;
-     });
+     var line = d3.line()
+       .x(function(val) { return x(val.Country); })
+       .y(function(val) { return y(val.Value); });
 
-     // Set up the scales
-     var xScale = d3.scaleBand()
+     var x = d3.scaleBand()
        .domain(yearData.map(function(d) { return d.Country; }))
        .range([0, width])
        .padding(0.1);
 
-     var yScale = d3.scaleLinear()
+     var y = d3.scaleLinear()
        .domain([0, d3.max(yearData, function(d) { return d.Value; })])
        .range([height, 0]);
 
-     // Define the line generator
-     var line = d3.line()
-       .x(function(d) { return xScale(d.Country) + xScale.bandwidth() / 2; })
-       .y(function(d) { return yScale(d.Value); });
-
-     // Update the line path
-     var linePath = svg.selectAll(".line-path")
-       .data([yearData]);
-
-     linePath.enter()
-       .append("path")
-       .attr("class", "line-path")
-       .merge(linePath)
-       .transition()
-       .duration(500)
-       .attr("d", line)
-       .style("fill", "none")
-       .style("stroke", "steelblue")
-       .style("stroke-width", "2px");
-
-     linePath.exit()
-       .remove();
-
-     // Add country labels
-     var labels = svg.selectAll(".label")
+       var labels = svg.selectAll(".label")
        .data(yearData);
 
      labels.enter()
        .append("text")
        .attr("class", "label")
        .merge(labels)
-       .attr("x", function(d) { return xScale(d.Country) + xScale.bandwidth() / 2; })
-       .attr("y", function(d) { return yScale(d.Value) - 5; })
        .attr("text-anchor", "middle")
+       .attr("x", function(d) { return x(d.Country) + 20; })
+       .attr("y", function(d) { return y(d.Value) - 5; })
        .text(function(d) { return d.Country + " - " + d.Value; });
 
      labels.exit()
        .remove();
 
-     // Update the x-axis
-     svg.select(".x-axis")
-       .attr("transform", "translate(0," + height + ")")
-       .call(d3.axisBottom(xScale))
-       .selectAll("text")
-       .style("text-anchor", "end")
-       .attr("dx", "-.8em")
-       .attr("dy", ".15em")
-       .attr("transform", "rotate(-65)");
 
-     // Update the y-axis
+     var linePath = svg.selectAll(".path")
+       .data([yearData]);
+
+     linePath.enter()
+       .append("path")
+       .attr("class", "path")
+       .merge(linePath)
+       .transition()
+       .duration(500)
+       .attr("d", line)
+       .style("fill", "none")
+       .style("stroke", "lightblue");
+
+     linePath.exit()
+       .remove();
+
+       svg.select(".x-axis")
+       .call(d3.axisBottom(x))
+       .selectAll("text")
+       .style("text-anchor", "end");
+
      svg.select(".y-axis")
-       .call(d3.axisLeft(yScale));
+       .call(d3.axisLeft(y));
    }
 
-   // Call the updateChart function with initial value
-   updateChart(selectedYear);
+   updateLineGraph(currentActiveYear);
  }).catch(function(error) {
-   console.log("Error loading data: " + error);
+   console.log("Unhandled error");
  });
